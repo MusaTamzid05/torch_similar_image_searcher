b8d1442f1a6181f00f5d3ecd669f005c74cb9e7b
MusaTamzid05 <tamzid101@gmail.com>
2022-01-17 15:28:59 +0600

    num_classes now needs to be send to the model.

--- searcher/models.py
+++ searcher/models.py
@@ -3,8 +3,14 @@ import torch
 
 import torch.nn.functional as F
 
+try:
+    from searcher.data_handler import CustomImageDataset
+except ModuleNotFoundError:
+    from data_handler import CustomImageDataset
+
+
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 10, 3, stride = 2)
         self.conv2 = nn.Conv2d(10, 20, 3, stride = 2)
@@ -13,6 +19,9 @@ class Net(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
 
         self.fc1 = nn.Linear(270, 100)
+        self.fc2 = nn.Linear(100, num_classes)
+
+        self.last_activation = nn.Softmax(dim = 1)
 
 
 
@@ -34,5 +43,22 @@ class Net(nn.Module):
 
         x = torch.flatten(x, 1)
         x = self.fc1(x)
+        x = self.fc2(x)
 
+        x = self.last_activation(x)
         return x
+
+
+
+if __name__ == "__main__":
+    dataset = CustomImageDataset(dir_path = "/home/musa/data/images/natural_images/data/natural_images", validation_dataset = True)
+    item = dataset[0]["src"]
+    #print(item.shape)
+
+    item = item.expand(torch.Size([1, 1, 256, 256]))
+    net = Net(num_classes = 5)
+    result = net.forward(item)
+
+    print(result.shape)
+    print(result)
+
