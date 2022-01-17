a46777394b3bdb1083947301cfea327d8d309e25
MusaTamzid05 <tamzid101@gmail.com>
2022-01-17 15:54:31 +0600

    training code refactor.

--- searcher/classifier.py
+++ searcher/classifier.py
@@ -15,41 +15,48 @@ class Classifier:
 
         self.device = "cuda" if torch.cuda.is_available() else  "cpu"
 
+
+    def _train(self):
+        training_losses = []
+
+        for train_data in self.train_data_loader:
+            x_batch = train_data["src"].to(self.device)
+            y_batch = train_data["label"].to(self.device)
+
+            self.model.train()
+            yhat = self.model(x_batch)
+
+            loss = self.loss_fn(y_batch, yhat)
+            loss.backward()
+
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            training_losses.append(loss.item())
+
+        return np.mean(training_losses)
+
+
+
     def fit(self, epochs = 100,  batch_size = 16):
         num_classes = len(self.train_dataset[0]["label"])
 
         self.train_data_loader = DataLoader(self.train_dataset, batch_size = batch_size)
         self.validation_data_loader = DataLoader(self.validation_dataset)
 
-        model = Net(num_classes = num_classes)
-        model.to(self.device)
+        self.model = Net(num_classes = num_classes)
+        self.model.to(self.device)
 
 
 
-        optimizer = optim.Adam(model.parameters(), lr = 0.01)
-        loss_fn = nn.MSELoss(reduction = "mean")
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.01)
+        self.loss_fn = nn.MSELoss(reduction = "mean")
 
         print("Training started")
 
+
         for epoch in range(epochs):
-            training_losses = []
-
-            for train_data in self.train_data_loader:
-                x_batch = train_data["src"].to(self.device)
-                y_batch = train_data["label"].to(self.device)
-
-                model.train()
-                yhat = model(x_batch)
-
-                loss = loss_fn(y_batch, yhat)
-                loss.backward()
-
-                optimizer.step()
-                optimizer.zero_grad()
-
-                training_losses.append(loss.item())
-
-
-            print(f"Epoch {epoch} Training loss : {np.mean(training_losses)}")
+            training_loss  = self._train()
+            print(f"Epoch {epoch} Training loss : {training_loss}")
 
 
