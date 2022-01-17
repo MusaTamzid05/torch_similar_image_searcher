ee84df0e3b251dc6e77c127f39b10385699a4e58
MusaTamzid05 <tamzid101@gmail.com>
2022-01-17 13:22:16 +0600

    label is now set in the dataset.

--- searcher/data_handler.py
+++ searcher/data_handler.py
@@ -5,6 +5,7 @@ import numpy as np
 from torchvision import transforms
 
 import torch
+import random
 
 
 class Resize:
@@ -30,11 +31,7 @@ class ToTensor:
 class CustomImageDataset(Dataset):
     def __init__(self,  dir_path, validation_dataset = False, size = 256):
         self._load(dir_path = dir_path, validation_dataset = validation_dataset)
-
         self.transforms = transforms.Compose([Resize(size = size), Normalize(), ToTensor()])
-
-
-
 
 
     def __len__(self):
@@ -42,15 +39,30 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         image_path = self.image_paths[idx]
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
+        label = self.labels[idx]
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
         processed_image = self.transforms(image)
 
-
-        item = {"src" : processed_image , "dst" : processed_image}
+        item = {"src" : processed_image , "label" : torch.Tensor(label)}
 
         return item
+
+    def _shuffle(self):
+        shuffle_indexes = [i for i in range(len(self.image_paths))]
+        random.shuffle(shuffle_indexes)
+
+        X = []
+        y = []
+
+        for index in shuffle_indexes:
+            X.append(self.image_paths[index])
+            y.append(self.labels[index])
+
+
+        self.image_paths = X
+        self.labels = y
 
 
 
@@ -58,19 +70,32 @@ class CustomImageDataset(Dataset):
     def _load(self, dir_path, validation_dataset):
         label_names = sorted(os.listdir(dir_path))
         image_paths  = []
+        labels = []
 
-        for label_name in label_names:
+        for index, label_name in enumerate(label_names):
             label_dir_path = os.path.join(dir_path, label_name)
             current_paths = self._load_image_paths(image_dir_path = label_dir_path)
-
             image_paths += current_paths
+
+            encoded_label = [0 for _ in range(len(label_names))]
+            encoded_label[index] = 1
+
+            labels += [encoded_label for _ in range(len(current_paths))]
 
         train_index = int(0.8 * len(image_paths))
 
+        self.image_paths = image_paths
+        self.labels = labels
+
+        self._shuffle()
+
+
         if validation_dataset == False:
-            self.image_paths = image_paths[:train_index]
+            self.image_paths = self.image_paths[:train_index]
+            self.labels = self.labels[:train_index]
         else:
-            self.image_paths = image_paths[train_index:]
+            self.image_paths = self.image_paths[train_index:]
+            self.labels = self.labels[train_index:]
 
 
     def _load_image_paths(self, image_dir_path):
@@ -90,6 +115,7 @@ def main():
 
     for item in dataset:
         print(item["src"].shape)
+        print(item["label"])
 
 if __name__ == "__main__":
     main()
