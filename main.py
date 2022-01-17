c5ecc36dbc8fa00ab9b5bfb6a0457952924ba191
MusaTamzid05 <tamzid101@gmail.com>
2022-01-17 16:16:03 +0600

    comment removed.

--- main.py
+++ main.py
@@ -7,17 +7,6 @@ from searcher.classifier import Classifier
 
 
 if __name__ == "__main__":
-    '''
-    dataset = CustomImageDataset(dir_path = "/home/musa/data/images/natural_images/data/natural_images", validation_dataset = True)
-    item = dataset[0]["src"]
-
-    item = item.expand(torch.Size([1, 1, 256, 256]))
-    net = Net()
-    result = net.forward(item)
-
-    print(result.shape)
-    '''
-
     cls = Classifier(data_dir_path = "/home/musa/data/images/natural_images/data/natural_images")
     cls.fit()
 
