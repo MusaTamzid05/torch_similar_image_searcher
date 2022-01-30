b1f46382d3ae2eebc2560e61a7e750a93479c2e8
MusaTamzid05 <tamzid101@gmail.com>
2022-01-30 13:58:23 +0600

    argparse added.

---
+++ README.md
@@ -0,0 +1,1 @@
+python main.py -s /home/musa/datasets/natural_images/natural_images -t model_data    
--- main.py
+++ main.py
@@ -4,11 +4,20 @@ import numpy as np
 import torch
 
 from searcher.classifier import Classifier
+import argparse
 
 
 if __name__ == "__main__":
-    cls = Classifier(data_dir_path = "/home/musa/data/images/natural_images/data/natural_images")
-    cls.fit(epochs = 2)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--src_dst", type = str, help = "The src directory for training.", required = False)
+    parser.add_argument("-t", "--train_dir", type = str, help = "Training directory where te model will be saved", required = True)
+
+    args = parser.parse_args()
+
+
+    cls = Classifier(data_dir_path = args.src_dst)
+    cls.fit(epochs = 2, save_dir_path = args.train_dir)
 
 
 
